37f553abafd7efec2bd3fdc47804d848b14c9f57
EC2 Default User <ec2-user@ip-172-31-85-189.ec2.internal>
2022-11-19 17:01:19 +0000

    kube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -16,4 +16,4 @@ kubectl create deploy kube-proj --image=magdysalemm/kube_proj:v1.0.0
 kubectl get deploy,rs,svc,pods
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/kube-proj-7db945c845-dqqhq --address 0.0.0.0 8000:80
+kubectl port-forward pod/kube-proj-588488d5f-b6cpn --address 0.0.0.0 8000:80
